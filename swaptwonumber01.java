191d85f77ceffca12eca5fed87ce7cd28d9eff07
Akash Patil <akashgpatil8806@example.com>
2024-06-04 00:49:58 +0530

    swap two number without 3rd variable

---
+++ swaptwonumber01.java
@@ -0,0 +1,23 @@
+// swap two number
+public class swaptwonumber01 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Swapping logic
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    
+}
